0d2c5fe84f4700b457e8a865d833fcc857a4b613
Radhika Hambire <radhika.hambire@iauro.com>
2019-05-16 16:16:10 +0530

    testcase added

--- Anuroop hybrid app/Test Suites/LoginScenario.ts
+++ Anuroop hybrid app/Test Suites/LoginScenario.ts
@@ -11,12 +11,6 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>95a863c8-0281-4b95-8d5a-3397a63f4a59</testSuiteGuid>
    <testCaseLink>
-      <guid>ab782668-8666-4275-8b5a-bc79ba6337cb</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/To check functionality of Contacts Viewed Profile</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
       <guid>ba5dfe75-109d-4815-be81-126fafa0b6d0</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
@@ -27,5 +21,11 @@
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Checking_events</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>44a3f67b-3232-4eb4-91f3-7ea0c0af46ce</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/To_check_contacts_viewed_profile</testCaseId>
    </testCaseLink>
 </TestSuiteEntity>
